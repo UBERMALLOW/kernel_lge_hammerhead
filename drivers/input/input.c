c43ff9f6746441f9269aca66788f54043b7ba36d
Chet Kener <Cl3Kener@gmail.com>
2015-10-15 18:50:36 +0400

    Fix for "Long-press Volume Button Skips Tracks" Feature
    * With this long press feature Keys pressed at suspend time ARE likely to be pressed on resume.  Hopefully I didn't crush your dreams Google.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -1727,14 +1727,6 @@ void input_reset_device(struct input_dev *dev)
 
 	if (dev->users) {
 		input_dev_toggle(dev, true);
-
-		/*
-		 * Keys that have been pressed at suspend time are unlikely
-		 * to be still pressed when we resume.
-		 */
-		spin_lock_irq(&dev->event_lock);
-		input_dev_release_keys(dev);
-		spin_unlock_irq(&dev->event_lock);
 	}
 
 	mutex_unlock(&dev->mutex);
